c9238fbe0255fc5753a94eca4ebcec22cd1d7f0d
krapmot <krapmot@gmail.com>
2017-03-13 14:37:06 -0400

    Add files via upload

--- WAR4.ino
+++ WAR4.ino
@@ -1,11 +1,17 @@
 #include <AFMotor.h> //import your motor shield library
 #define trigPin A5// define the pins of your sensor
 #define echoPin A3
- #define trigPin1 A0
- #define echoPin1 A2
- #define trigPin2 A1
- #define echoPin2 A4 
- 
+#define trigPin1 A1
+#define echoPin1 A2
+#define led1 A0
+#define led2 A4
+
+// #define trigPin2 A1
+// #define echoPin2 A4 
+
+
+
+
 AF_DCMotor motor1(1,MOTOR12_64KHZ); // set up motors.
 AF_DCMotor motor2(2, MOTOR12_8KHZ);
  
@@ -14,13 +20,17 @@ void setup() {
   Serial.println("Motor test!");
   pinMode(trigPin, OUTPUT);// set the trig pin to output (Send sound waves)
   pinMode(echoPin, INPUT);// set the echo pin to input (recieve sound waves)
-pinMode(trigPin1, OUTPUT);// set the trig pin to output (Send sound waves)
+  pinMode(trigPin1, OUTPUT);// set the trig pin to output (Send sound waves)
   pinMode(echoPin1, INPUT);
-  pinMode(trigPin2, OUTPUT);// set the trig pin to output (Send sound waves)
-  pinMode(echoPin2, INPUT);
+  //pinMode(trigPin2, OUTPUT);// set the trig pin to output (Send sound waves)
+  //pinMode(echoPin2, INPUT);
   
   motor1.setSpeed(105); //set the speed of the motors, between 0-255
   motor2.setSpeed (130);  
+
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  
 }
 
 void moveStraight() {
@@ -29,45 +39,74 @@ void moveStraight() {
   }
 
  void turn() {
-  Serial.print ("Reorienting");
+  Serial.print ("Reorienting:\n");
   motor1.run(FORWARD);
   motor2.run(BACKWARD);
   delay(450);
   }
 
+void led1Set() {
+  digitalWrite(led1, HIGH);
+  delay(100);
+
+  digitalWrite(led1, LOW);
+  delay(100);
+  }
+
+void led2Set() {
+  
+  digitalWrite(led2, HIGH);
+  delay(100);
+  digitalWrite(led2, LOW);
+  delay(100);
+  }
+
+
+void infoMesh() {
+  Serial.println ("\n \t Close Obstacle detected!" );
+  Serial.println ("Obstacle Details:");
+  }
+  
 void loop() {
 
-  long duration, duration1, duration2, distance, distance1, distance2; // start the scan
+  long duration, duration1, distance, distance1; // start the scan
   
   digitalWrite(trigPin, LOW);  
-  digitalWrite(trigPin1, LOW);
-  digitalWrite(trigPin2, LOW);
   delayMicroseconds(2); // delays are required for a succesful sensor operation.
   digitalWrite(trigPin, HIGH);
-  digitalWrite(trigPin1, HIGH);
-  digitalWrite(trigPin2, HIGH);
   delayMicroseconds(5); //this delay is required as well!
   digitalWrite(trigPin, LOW);
-  digitalWrite(trigPin1, LOW);
-  digitalWrite(trigPin2, LOW);
   duration = pulseIn(echoPin, HIGH);
   distance = (duration/2) / 29.1;// convert the distance to centimeters.
-  duration1 = pulseIn(echoPin1, HIGH);
-  distance1 = (duration1/2) / 29.1;
-  duration2 = pulseIn(echoPin2, HIGH);
-  distance2 = (duration2/2) / 29.1;
-  
- if (distance <= 15 && distance1 <= 15 && distance2 <= 15  )/*if there's an obstacle 25 centimers, ahead, do the following: */ {   
-  Serial.print(duration);
-  Serial.println ("Close Obstacle detected!" );
-  Serial.println ("Obstacle Details:");
-  Serial.print ("Distance From Robot is " );
-  Serial.print ( distance);
-  Serial.print ( " CM!");// print out the distance in centimeters.
 
-  Serial.println (" \n ");
+  digitalWrite(trigPin1, LOW);  
+  delayMicroseconds(2); // delays are required for a succesful sensor operation.
+  digitalWrite(trigPin1, HIGH);
+  delayMicroseconds(5); //this delay is required as well!
+  digitalWrite(trigPin1, LOW);
+  duration1 = pulseIn(echoPin1, HIGH);
+  distance1 = (duration1/2) / 29.1;// convert the distance to centimeters.
+  
+ if (distance <= 15)/*if there's an obstacle 25 centimers, ahead, do the following: */ {   
+  infoMesh();
+  Serial.print ("Distance in CM from left: ");
+  Serial.print ( distance);
+  Serial.print("\n\n");
   turn();
+  led1Set();
+
 }
+
+ if (distance1 <= 15)/*if there's an obstacle 25 centimers, ahead, do the following: */ {   
+  infoMesh();
+  Serial.print ("Distance in CM from right: ");
+  Serial.print ( distance1);
+  Serial.print("\n\n");
+  turn();
+  led2Set();
+
+}
+ 
   else {
    moveStraight();
 }
